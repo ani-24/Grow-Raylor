db94a3eeee09c92c3f83283b346e6cc7136b92ad
Aniket <rajeshwarkumar9204@gmail.com>
2023-03-01 00:46:49 +0530

    change image urls

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      "https://63fe0e4a5a2c8a0007ab0838--peaceful-druid-f4126e.netlify.app/",
-    ],
+    domains: ["https://res.cloudinary.com/"],
   },
 };
 
