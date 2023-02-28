eea2d039109f16a83af4b706aa766971fe581145
Aniket <rajeshwarkumar9204@gmail.com>
2023-02-28 20:05:51 +0530

    change image location

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["https://main--quynhvo.netlify.app/"],
+    domains: [
+      "https://63fe0e4a5a2c8a0007ab0838--peaceful-druid-f4126e.netlify.app/",
+    ],
   },
 };
 
